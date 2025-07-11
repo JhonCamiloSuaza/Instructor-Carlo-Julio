472af233804ecb9eb2bbdc02710a4a13796aec7f
JhonCamiloSuaza <jhon04suazasanchez@gmail.com>
2025-07-11 11:24:10 -0500

    Delete Abstract Factory- Carlo Julio directory
    Todo los Ejercicios

--- Abstract Factory- Carlo Julio/FabricaAnimales.java
+++
@@ -1,4 +0,0 @@
-
-public interface FabricaAnimales {
-    Animal crearAnimal();
-}
--- Abstract Factory- Carlo Julio/Vaca.java
+++
@@ -1,8 +0,0 @@
-
-public class Vaca implements Animal {
-    public void sonido() {
-        System.out.println("La vaca dice muuu ");
-    }
-}
-
-
